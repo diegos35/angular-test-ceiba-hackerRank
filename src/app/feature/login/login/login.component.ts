ffe74d5dc1e4bc61dbda7871c0a655037d8c09b7
diego <diego_1996.c.c@gmail.com>
2023-09-17 21:12:49 -0500

    ajuste

--- src/app/feature/login/login/login.component.ts
+++ src/app/feature/login/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit{
       if (this.loginForm.valid) {
         const { email, password } = await this.loginForm.value;
         const {token} = await this.loginService.login(email,password);
-          //localStorage.setItem('token', token);
+          localStorage.setItem('token', token);
           this.redirectUsers();
       }else{
         this.loginForm.markAllAsTouched()
